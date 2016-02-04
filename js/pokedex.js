86ba034793ba6b40acf35b3f156dfa4d8f89b861
kobes145 <kobesmith145@gmail.com>
2016-02-04 23:13:56 +0000

    kobe-san

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,26 +1,36 @@
 $(function() {
   // Step 0: Set your assigned pokemon to the "pokemon" variable! I have put in Bulbasaur for an example
-  var pokemon = bulbasaur;
+  var pokemon = squirtle;
 
   // STEP 1: Uncomment the next line and make sure that the pokemon object prints to the console!
-  //   e.g. console.log(pokemon)
+  console.log(pokemon.abilities[0].name);
 
   // STEP 2: Use jQuery to add information to the empty fields. The first few have been done for you.
 
   $(".pokemon-name").append(pokemon.name);
 
 
-  $(".pokemon-species").append("something");
-  $(".pokemon-evolves").append("something else");
+  $(".pokemon-species").append(pokemon.species);
+  $(".pokemon-evolves").append(pokemon.evolutions.to);
 
   // TODO: Add evolution level here
+   $(".pokemon-ev-level").append(pokemon.evolutions[0].level);
   // TODO: Add speed here
+   $(".pokemon-spd").append(pokemon.speed);
   // TODO: Add attack here
+   $(".pokemon-atk").append(pokemon.attack);
   // TODO: Add defense here
+   $(".pokemon-def").append(pokemon.defense);
   // TODO: Add ability names here
+   for(var i=0;pokemon.abilities.length;i++){
+      $(".pokemon-evolves").append("<li>" + pokemon.abilities[0].name + "</li>");
+    }
   // TODO: Add egg group names here
-
+    for(var i=0;pokemon.egg_groups.length;i++){
+        $(".pokemon-evolves").append(pokemon.egg_groups[i].name);
+      }
   // TODO: Add move names in alphabetical order here
+    
   // TODO: Add ONLY level up move names here
 
   // Now... take a deep breath and go back to your move lists.
@@ -39,7 +49,7 @@ $(function() {
   var getProperImageURL = function(pokemon) {
     var pokemonNumber = pokemon.pkdx_id;
     var urlNum = returnThreeDigits(pokemonNumber);
-    return "http://assets22.pokemon.com/assets/cms2/img/pokedex/full/" + urlNum + ".png";
+    return "https://assets22.pokemon.com/assets/cms2/img/pokedex/full/" + urlNum + ".png";
   };
   
   var returnThreeDigits = function(num) {
